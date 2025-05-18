3488aff8d4c92916a5a289e66a69be5850a00754
faniloo08 <rabemanantsoafanilo8@gmail.com>
2025-05-18 15:18:08 +0200

    feat: ajout du backend strapi

---
+++ backend/src/api/utilisateur/controllers/utilisateur.ts
@@ -0,0 +1,7 @@
+/**
+ * utilisateur controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::utilisateur.utilisateur');
---
+++ backend/src/api/utilisateur/routes/utilisateur.ts
@@ -0,0 +1,7 @@
+/**
+ * utilisateur router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::utilisateur.utilisateur');
