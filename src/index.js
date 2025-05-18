00e9040c03157c7bfce1d517e5a470112eefd74d
faniloo08 <rabemanantsoafanilo8@gmail.com>
2025-05-18 12:46:17 +0200

    feat: création des interfacaes landing et Home

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('node:path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+let mainWindow; // Déclaration globale
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 1728,
+    height: 969,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false, 
+      contextIsolation: true
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.webContents.openDevTools();
+};
+
+// Gestion du changement de page via IPC
+ipcMain.on('navigate-to', (event, page) => {
+  if (mainWindow) {
+    mainWindow.loadFile(`src/${page}.html`);
+  }
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
---
+++ src/preload.js
@@ -0,0 +1,15 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const  { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
+  // we can also expose variables, not just functions
+  
+})
+
+contextBridge.exposeInMainWorld('electron', {
+  navigateTo: (page) => ipcRenderer.send('navigate-to', page)
+});
