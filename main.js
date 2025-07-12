9e933bb277b258e824defcebea5949e40df76a3a
faniloo08 <rabemanantsoafanilo8@gmail.com>
2025-07-12 14:16:22 +0200

    modification de main

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    
     webPreferences: {
       preload: path.join(__dirname, 'src/preload.js'),
       contextIsolation: true,
